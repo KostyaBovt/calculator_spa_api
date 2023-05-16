073044dce2d6156584488c5d87d70d4dd4c08c71
KostyaBovt <kostya.bovt@gmail.com>
2023-05-16 20:40:55 +0300

    wip on front

--- front/app.js
+++ front/app.js
@@ -3,6 +3,22 @@ window.onload = function() {
 	var countButton = document.getElementById('count');
 
 	countButton.onclick = function(event) {
-		alert('works!')
+		let postObj = {"arg_1": 10, "arg_2": 20}
+		let post = JSON.stringify(postObj)
+		const url = "http://localhost:5000/add"
+		let xhr = new XMLHttpRequest()
+		xhr.open('POST', url, true)
+		xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+		xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+		xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS')
+		xhr.setRequestHeader('Access-Control-Allow-Headers', 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization')
+
+		xhr.send(post);
+		xhr.onload = function () {
+			if(xhr.status === 200) {
+				console.log("Post successfully created!") 
+				console.log(xhr) 
+			}
+		}
 	}
 }
